163cc19e75d831514ba65e89a6ecf73c1d0c8705
Kolotylo Kyrylo <kolot102@gmail.com>
2024-03-06 12:13:23 +0200

    init commit

---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.scss'
+import { routeTree } from './routeTree.gen'
+import { RouterProvider, createRouter } from '@tanstack/react-router'
+
+const router = createRouter({ routeTree })
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
---
+++ src/routes/index.lazy.tsx
@@ -0,0 +1,13 @@
+import { createLazyFileRoute } from '@tanstack/react-router'
+
+export const Route = createLazyFileRoute('/')({
+  component: Index,
+})
+
+function Index() {
+  return (
+    <div>
+      <h3>Welcome Home!</h3>
+    </div>
+  )
+}
