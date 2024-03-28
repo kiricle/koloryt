fb5f3cc2e62d22c18aa34a556274f37ace0e49fa
Kolotylo Kyrylo <kolot102@gmail.com>
2024-03-28 18:57:19 +0200

    burger menu bugfix

--- src/components/BurgerMenu/BurgerMenu.tsx
+++ src/components/BurgerMenu/BurgerMenu.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import styles from './BurgerMenu.module.scss';
 import { links } from '../../utils/links';
 import { Link } from '@tanstack/react-router';
+import { Logo } from '../Logo/Logo';
 
 export const BurgerMenu = () => {
     const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -22,6 +23,21 @@ export const BurgerMenu = () => {
             <nav
                 className={[styles.menu, isOpen && styles.menu_open].join(' ')}
             >
+                <div className={styles.logo_container}>
+                    <h2 className={styles.title}>Koloryt</h2>
+                    <button
+                        className={[
+                            styles.burger,
+                            isOpen && styles.button_open,
+                        ].join(' ')}
+                        onClick={() => setIsOpen((prev) => !prev)}
+                    >
+                        <div className={styles.bar1}></div>
+                        <div className={styles.bar2}></div>
+                        <div className={styles.bar3}></div>
+                    </button>
+                </div>
+
                 {links.map(({ href, name }) => (
                     <Link
                         key={name}
